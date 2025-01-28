dcd3217df223ce63575564f74e1c688478b62235
Emma Blencowe <emma.blencowe95@gmail.com>
2025-01-28 20:25:10 +0000

    Uploaded all files for webpage

---
+++ script.js
@@ -0,0 +1,28 @@
+// Nav Scroll
+let nav = document.querySelector('nav');
+   
+    window.addEventListener('scroll', ()=>{
+        nav.classList.toggle('nav-scroll', window.scrollY > 0);
+    });
+
+// Active Toggle
+let AllNavLinkA = document.querySelectorAll('.nav-links li a ');
+
+// Active Add
+AllNavLinkA.forEach(item=>{
+    item.addEventListener('click',()=>{
+        removeActive();
+        item.classList.add('active');
+    });
+});
+// Active Remove
+const removeActive =()=> AllNavLinkA.forEach(item=>{
+    item.classList.remove('active');
+});
+
+// Nav Links Toggler
+let menuBars = document.querySelector('nav button');
+let navlinks = document.querySelector('.nav-links')
+menuBars.addEventListener('click',()=>{
+    navlinks.classList.toggle('nav-link-block')
+});
